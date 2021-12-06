88bde11fde741f477d0e15f1fa7a5dbc8e6c5808
Cwonseok <9617081@gmail.com>
2021-12-06 17:56:56 +0900

    5.2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import "./App.css";
 import AddNumberRoot from "./components/AddNumberRoot";
 import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Root</h1>
-      <AddNumberRoot></AddNumberRoot>
-      <DisplayNumberRoot></DisplayNumberRoot>
-    </div>
-  );
+class App extends Component {
+  state = { number: 0 };
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot
+        // onClick={function (size) {
+        //   this.setState({ number: this.state.number + size });
+        // }.bind(this)} // 리덕스로 전환하였기 떄문에 필요 없다.
+        ></AddNumberRoot>
+        <DisplayNumberRoot
+        //  number={this.state.number} // 
+        ></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+// import store from '../store';
 
 export default class AddNumber extends Component {
+  state = { size: 1 };
   render() {
     return (
       <div>
         <h1>Add Number</h1>
-        <input type="button" value="+"></input>
-        <input type="text" value="0"></input>
+        <input
+          type="button"
+          value="+"
+          onClick={function () {
+            this.props.onClick(this.state.size); //리덕스 적용 전의 코드
+            // store.dispatch({type:'INCREMENT', size:this.state.size}); // 리덕스 종속 코드
+          }.bind(this)}
+        ></input>
+        <input
+          type="text" 
+          value={this.state.size}
+          onChange={function (e) {
+            this.setState({ size: Number(e.target.value) });
+          }.bind(this)}
+        ></input>
       </div>
     );
   }
