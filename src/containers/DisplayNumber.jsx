c8d9136b65be064d584c527cec2586e201c52558
Cwonseok <85975787+Cwonseok@users.noreply.github.com>
2021-12-13 20:58:01 +0900

    6-2

--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,19 +1,34 @@
 import DisplayNumber from '../components/DisplayNumber';
-import React, { Component } from 'react';
-import store from '../store';
 
-export default class extends Component {
-	state = { number: store.getState().number };
-	constructor(props) {
-		// componentDidMount(props) {
-		super(props);
-		store.subscribe(
-			function () {
-				this.setState({ number: store.getState().number });
-			}.bind(this),
-		);
-	}
-	render() {
-		return <DisplayNumber number={this.state.number} unit={this.props.unit}></DisplayNumber>;
-	}
-}
+// redux 문구로 인하여 자동 익스포드 디폴트 해준다 
+// 전에는 수동으로 displaynumber 에서 import store 해주었지만 redux 화로 인하여 index.js 에서 해주면 끝이다.
+import {connect} from 'react-redux';
+export default connect()(DisplayNumber);
+// connect 란 함수가
+// 	render() {
+// 		return <DisplayNumber number={this.state.number} unit={this.props.unit}></DisplayNumber>;
+// 	}
+// 이런 것을 자동으로 뱉어내줌 
+
+
+
+
+
+// import React, { Component } from 'react';
+// import store from '../store';
+
+// export default class extends Component {
+// 	state = { number: store.getState().number };
+// 	constructor(props) {
+// 		// componentDidMount(props) {
+// 		super(props);
+// 		store.subscribe(
+// 			function () {
+// 				this.setState({ number: store.getState().number });
+// 			}.bind(this),
+// 		);
+// 	}
+// 	render() {
+// 		return <DisplayNumber number={this.state.number} unit={this.props.unit}></DisplayNumber>;
+// 	}
+// }
